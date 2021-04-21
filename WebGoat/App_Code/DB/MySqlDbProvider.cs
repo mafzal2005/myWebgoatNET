bfab8e29c10d96517c278885d31537a376558f6d
mafzal2005 <mafzal2005@gmail.com>
2021-04-21 19:03:41 -0400

    change framework3

--- WebGoat/App_Code/DB/MySqlDbProvider.cs
+++ WebGoat/App_Code/DB/MySqlDbProvider.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Data;
-using MySql.Data.MySqlClient;
+/// using MySql.Data.MySqlClient;
+using System.Data.SQLite;
 using log4net;
 using System.Reflection;
 using System.Diagnostics;
